7108eb618850be56219519f3201cfe911771f4f4
Joel Gonzalez <joel202590@outlook.com>
2025-03-20 08:20:47 -0600

    Se modificaron todas las paginas de Usuario y tambien el inicio de session

--- JFGM20250320.AppWebMVC/Controllers/UsersController.cs
+++ JFGM20250320.AppWebMVC/Controllers/UsersController.cs
@@ -6,6 +6,12 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using JFGM20250320.AppWebMVC.Models;
+using System.Security.Cryptography;
+using System.Text;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using System.Security.Claims;
 
 namespace JFGM20250320.AppWebMVC.Controllers
 {
@@ -24,7 +30,6 @@ namespace JFGM20250320.AppWebMVC.Controllers
             return View(await _context.Users.ToListAsync());
         }
 
-        // GET: Users/Details/5
         public async Task<IActionResult> Details(int? id)
         {
             if (id == null)
@@ -42,21 +47,20 @@ namespace JFGM20250320.AppWebMVC.Controllers
             return View(user);
         }
 
-        // GET: Users/Create
+
         public IActionResult Create()
         {
             return View();
         }
 
-        // POST: Users/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("UserId,Username,Email,PasswordHash,Role")] User user)
+        public async Task<IActionResult> Create([Bind("UserId,Username,Email,PasswordHash,paswordConfir,Role")] User user)
         {
             if (ModelState.IsValid)
             {
+                user.PasswordHash = CalcularHashMD5(user.PasswordHash);
                 _context.Add(user);
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
@@ -64,7 +68,7 @@ namespace JFGM20250320.AppWebMVC.Controllers
             return View(user);
         }
 
-        // GET: Users/Edit/5
+
         public async Task<IActionResult> Edit(int? id)
         {
             if (id == null)
@@ -80,42 +84,42 @@ namespace JFGM20250320.AppWebMVC.Controllers
             return View(user);
         }
 
-        // POST: Users/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("UserId,Username,Email,PasswordHash,Role")] User user)
+        public async Task<IActionResult> Edit(int id, [Bind("UserId,Username,Email,Role")] User user)
         {
             if (id != user.UserId)
             {
                 return NotFound();
             }
 
-            if (ModelState.IsValid)
+            var userUpdate = await _context.Users.FirstOrDefaultAsync(m => m.UserId == user.UserId);
+            try
             {
-                try
-                {
-                    _context.Update(user);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!UserExists(user.UserId))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
+                userUpdate.Username = user.Username;
+                userUpdate.Email = user.Email;
+                userUpdate.Role = user.Role;
+                _context.Update(userUpdate);
+                await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
-            return View(user);
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!UserExists(user.UserId))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+
+
         }
 
-        // GET: Users/Delete/5
         public async Task<IActionResult> Delete(int? id)
         {
             if (id == null)
@@ -133,7 +137,7 @@ namespace JFGM20250320.AppWebMVC.Controllers
             return View(user);
         }
 
-        // POST: Users/Delete/5
+
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
@@ -152,5 +156,103 @@ namespace JFGM20250320.AppWebMVC.Controllers
         {
             return _context.Users.Any(e => e.UserId == id);
         }
+
+        public async Task<IActionResult> CerrarSession()
+        {
+ 
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return RedirectToAction("Index", "Home");
+        }
+
+        [AllowAnonymous]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public async Task<IActionResult> Login(User user)
+        {
+            user.PasswordHash = CalcularHashMD5(user.PasswordHash);
+            var usuarioAuth = await _context.
+                Users.
+                FirstOrDefaultAsync(s => s.Email == user.Email && s.PasswordHash == user.PasswordHash);
+            if (usuarioAuth != null && usuarioAuth.UserId > 0 && usuarioAuth.Email == user.Email)
+            {
+                var claims = new[] {
+        new Claim(ClaimTypes.Name, usuarioAuth.Email),
+        new Claim("UserId", usuarioAuth.UserId.ToString()),
+         new Claim("Username", usuarioAuth.Username),
+        new Claim(ClaimTypes.Role, usuarioAuth.Role)
+        };
+                var identity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+                await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(identity));
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ModelState.AddModelError("", "El email o contraseña estan incorrectos");
+                return View();
+            }
+        }
+
+        public async Task<IActionResult> Perfil()
+        {
+
+            var idStr = User.FindFirst("UserId")?.Value;
+            int id = int.Parse(idStr);
+            var user = await _context.Users.FindAsync(id);
+            return View(user);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Perfil(int id, [Bind("UserId,Username,Email,PasswordHash,paswordConfir,Role")] User user)
+        {
+            if (id != user.UserId)
+            {
+                return NotFound();
+            }
+            var userUpdate = await _context.Users.FirstOrDefaultAsync(m => m.UserId == user.UserId);
+            try
+            {
+                userUpdate.Username = user.Username;
+                userUpdate.Email = user.Email;
+                userUpdate.PasswordHash = CalcularHashMD5(user.PasswordHash);
+                userUpdate.Role = user.Role;
+                _context.Update(userUpdate);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index", "Home");
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!UserExists(user.UserId))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    return View(user);
+                }
+            }
+        }
+
+        private string CalcularHashMD5(string input)
+        {
+            using (MD5 md5 = MD5.Create())
+            {
+                byte[] inputBytes = Encoding.UTF8.GetBytes(input);
+                byte[] hashBytes = md5.ComputeHash(inputBytes);
+
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < hashBytes.Length; i++)
+                {
+                    sb.Append(hashBytes[i].ToString("x2")); 
+                }
+                return sb.ToString();
+            }
+        }
+
+
     }
 }
--- JFGM20250320.AppWebMVC/Models/User.cs
+++ JFGM20250320.AppWebMVC/Models/User.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 
 namespace JFGM20250320.AppWebMVC.Models;
 
@@ -7,11 +9,28 @@ public partial class User
 {
     public int UserId { get; set; }
 
+    [Display(Name = "Nombre del usuario")]
+    [Required(ErrorMessage = "El nombre es obligatorio")]
     public string Username { get; set; } = null!;
 
+    [Display(Name = "Correo electronico")]
+    [Required(ErrorMessage = "El Correo es obligatorio")]
     public string Email { get; set; } = null!;
 
+    [Display(Name = "Contraseña")]
+    [Required(ErrorMessage = "La contraseña es obligatoria")]
+    [DataType(DataType.Password)]
+    [StringLength(40, MinimumLength = 5, ErrorMessage = "El passowrd debe tener entre 5 y 50 caracteres.")]
     public string PasswordHash { get; set; } = null!;
 
+    [Display(Name = "Rol")]
+    [Required(ErrorMessage = "El Rol es obligatorio")]
     public string Role { get; set; } = null!;
+
+    [NotMapped]
+    [StringLength(40, MinimumLength = 5, ErrorMessage = "El password debe tener entre 5 y 50 caracteres.")]
+    [Display(Name = "Confirmar Contraseña")]
+    [DataType(DataType.Password)]
+    [Compare("PasswordHash", ErrorMessage = "Las contraseñas no coinciden.")]
+    public string? paswordConfir { get; set; } = null!;
 }
